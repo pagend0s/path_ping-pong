1e99a65b44834c7945167a83e6430cd9b428f389
pagend0s <65870887+pagend0s@users.noreply.github.com>
2021-07-28 13:49:06 +0200

    Add files via upload

---
+++ path_ping-pong.ps1
@@ -0,0 +1,22 @@
+﻿function path_ping-pong(){
+Param
+(
+    [Parameter(Mandatory=$true, Position=0)]
+    [string]$IP4,
+
+    [Parameter(Mandatory=$true, Position=1)]
+    [Int]$Size
+)
+
+$pathping_result = pathping /n $IP4
+
+$IP = @( ($pathping_result  |  Select-String -Pattern "\d{1,3}(\.\d{1,3}){3}" -AllMatches).Matches.Value | Sort-Object | Get-Unique )
+
+Write-Output $pathping_result
+
+foreach($a in $IP ) {
+ping -n 2 -l $Size $a -f  
+} 
+
+}
+path_ping-pong -IP4 "SERVER" -Size "BYTES"
